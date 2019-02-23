81bd54c3abc2d5b8500dcc9b1f5f4e53ee2a3899
Shane Kimble <shanekimble12@Hotmail.com>
2019-02-23 14:17:20 -0500

    Update 2_update.sh

--- Debian/2_update.sh
+++ Debian/2_update.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 # Run as root
 
-# Ensure the PATH variable is correct or else this could fail
-source /etc/profile
-
 # Switch to backports kernel, get latest firmware, and install firewall
 apt-get update
 apt-get -t stretch-backports install intel-microcode \
     firmware-realtek linux-image-amd64 ufw -y
-apt-get dist-upgrade -y
+# apt-get -t stretch-backports install firmware-iwlwifi -y
+apt-get -t stretch-backports dist-upgrade -y
 ufw enable
