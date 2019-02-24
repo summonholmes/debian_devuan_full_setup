a340c2efb2bdcb1c7148848a1152321784d08fdc
Shane Kimble <shanekimble12@Hotmail.com>
2019-02-23 21:29:35 -0500

    Update 2_update.sh

--- Debian/2_update.sh
+++ Debian/2_update.sh
@@ -3,8 +3,7 @@
 
 # Switch to backports kernel, get latest firmware, and install firewall
 apt-get update
-apt-get -t stretch-backports install intel-microcode \
-    firmware-realtek linux-image-amd64 ufw -y
-# apt-get -t stretch-backports install firmware-iwlwifi -y
+apt-get -t stretch-backports install amd64-microcode firmware-realtek ufw -y
+# apt-get -t stretch-backports install intel-microcode firmware-iwlwifi -y
 apt-get -t stretch-backports dist-upgrade -y
 ufw enable
